4fbb4687d807693ee1e8cb6c5302532149779351
Gebesht_Andrei <andrey.gebesht.05@mail.ru>
2026-03-30 19:38:26 +0300

    fix BuildGame.cs to not generate empty directory

--- Assets/Editor/BuildGame.cs
+++ Assets/Editor/BuildGame.cs
@@ -1,28 +1,28 @@
 ﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
 using System.IO;
 
-public static class BuildScript
+public static class BuildGame
 {
+    [MenuItem("Build/Build Windows")]
     public static void BuildWindows()
     {
         string buildPath = Path.Combine("build", "StandaloneWindows64");
         if (!Directory.Exists(buildPath))
             Directory.CreateDirectory(buildPath);
-        string[] scenes = {
-            "Assets/Scenes/SampleScene.unity"
-        };
+        
         string exePath = Path.Combine(buildPath, "Lenfilm.exe");
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
-        {
-            scenes = scenes,
-            locationPathName = exePath,
-            target = BuildTarget.StandaloneWindows64,
-            options = BuildOptions.None
-        };
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
-        if (File.Exists(exePath))
+        
+        BuildReport report = BuildPipeline.BuildPlayer(
+            EditorBuildSettings.scenes,
+            exePath,
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+        
+        if (report.summary.result == BuildResult.Succeeded)
             UnityEngine.Debug.Log("Build succeeded: " + exePath);
         else
-            UnityEngine.Debug.LogError("Build failed! Check the log for errors.");
+            UnityEngine.Debug.LogError("Build failed! Result: " + report.summary.result);
     }
 }
