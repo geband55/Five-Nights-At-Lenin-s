885387f3bdff247a54a3e320ed3315662c00f715
Gebesht_Andrei <andrey.gebesht.05@mail.ru>
2026-03-30 18:38:18 +0300

    add game build script

---
+++ Assets/Editor/BuildGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using System.IO;
+
+public static class BuildScript
+{
+    public static void BuildWindows()
+    {
+        string buildPath = Path.Combine("build", "StandaloneWindows64");
+        if (!Directory.Exists(buildPath))
+            Directory.CreateDirectory(buildPath);
+        string[] scenes = {
+            "Assets/Scenes/SampleScene.unity"
+        };
+        string exePath = Path.Combine(buildPath, "Lenfilm.exe");
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = exePath,
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.None
+        };
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (File.Exists(exePath))
+            UnityEngine.Debug.Log("Build succeeded: " + exePath);
+        else
+            UnityEngine.Debug.LogError("Build failed! Check the log for errors.");
+    }
+}
